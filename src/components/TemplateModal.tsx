e54989efb0394092e0c3de2203b8b042f4b998d1
Amit Buskila <amit1bus@gmail.com>
2025-01-17 23:51:41 +0200

    add the coolest animation on set swipe

--- src/components/TemplateModal.tsx
+++ src/components/TemplateModal.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from "react";
 import { FieldErrors, useWatch } from "react-hook-form";
-import { ScrollView, View } from "react-native";
+import { KeyboardAvoidingView, ScrollView, View } from "react-native";
+import Animated from "react-native-reanimated";
 import {
   FormValues,
   useWorkoutFormContext,
 } from "../contexts/WorkoutForm.context";
+import { useSetListAnimation } from "../hooks/setAnimation.hook";
 import { CustomButton } from "./CustomButton";
 import { CustomImagePicker } from "./CustomImagePicker";
 import { FormField } from "./FormField";
@@ -20,7 +22,7 @@ export const TemplateModal: FC<{}> = () => {
     formState: { errors },
   } = useWorkoutFormContext().form;
 
-  const [index] = useWorkoutFormContext().currExerciseIndex;
+  const [exerciseIndex] = useWorkoutFormContext().currExerciseIndex;
 
   const exercises = useWatch({ control, name: "exercises" });
 
@@ -32,35 +34,43 @@ export const TemplateModal: FC<{}> = () => {
     console.log("Error occurred", errors);
   };
 
+  const animatedStyle = useSetListAnimation({ exerciseIndex, exercises });
+
   return (
-    <ScrollView className="bg-primary h-full">
-      <View className="w-full justify-center h-full px-4 my-4">
-        <FormField
-          title="Workout Name"
-          control={control}
-          styles="w-full h-14"
-        />
-        <View className="my-2 flex flex-row items-center">
+    <KeyboardAvoidingView behavior={"position"}>
+      <ScrollView className="bg-primary h-full">
+        <View className="w-full justify-center h-full px-4 my-4">
           <FormField
-            title="Description"
+            title="Workout Name"
             control={control}
-            styles="w-[66vw] h-28"
-            multiline={true}
+            styles="w-full h-14"
           />
-          <CustomImagePicker
-            styles="w-[24vw] h-28 mx-4 mt-5"
-            setValue={setValue}
-            uri={getValues("image")}
+          <View className="my-2 flex flex-row items-center">
+            <FormField
+              title="Description"
+              control={control}
+              styles="w-[66vw] h-28"
+              multiline={true}
+            />
+            <CustomImagePicker
+              styles="w-[24vw] h-28 mx-4 mt-5"
+              setValue={setValue}
+              uri={getValues("image")}
+            />
+          </View>
+          <WorkoutCarousel />
+          {!!exercises[exerciseIndex]?.sets && (
+            <Animated.View style={animatedStyle}>
+              <SetList isWorkout={true} exerciseIndex={exerciseIndex} />
+            </Animated.View>
+          )}
+
+          <CustomButton
+            title="Create Template"
+            handlePress={handleSubmit(onSubmit, onError)}
           />
         </View>
-        <WorkoutCarousel />
-        {exercises[index] && <SetList isWorkout={true} index={index} />}
-
-        <CustomButton
-          title="Create Template"
-          handlePress={handleSubmit(onSubmit, onError)}
-        />
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
