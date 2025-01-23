4d408e03ab122bb5c934a74de7ba89f8a9279f7f
Amit Buskila <amit1bus@gmail.com>
2025-01-24 00:44:20 +0200

    return reanimated component

--- src/components/Workouts/WorkoutRoutine.tsx
+++ src/components/Workouts/WorkoutRoutine.tsx
@@ -1,11 +1,12 @@
 import { useWatch } from "react-hook-form";
-import { Animated, View } from "react-native";
+import { View } from "react-native";
 import { useWorkoutFormContext } from "../../contexts/WorkoutForm.context";
 import { useSetListAnimation } from "../../hooks/setAnimation.hook";
 import { SetList } from "./SetList/SetList";
 import { WorkoutCarousel } from "./WorkoutCarousel/WorkoutCarousel";
 import { FC } from "react";
 import { Workout } from "../../../types/template";
+import Animatable from "react-native-reanimated";
 
 export const WorkoutRoutine: FC<{ workout?: Workout }> = ({ workout }) => {
   const [exerciseIndex] = useWorkoutFormContext().currExerciseIndex;
@@ -15,11 +16,13 @@ export const WorkoutRoutine: FC<{ workout?: Workout }> = ({ workout }) => {
   return (
     <View>
       <WorkoutCarousel />
-      {!!exercises[exerciseIndex]?.sets && (
-        <Animated.View style={animatedStyle}>
-          <SetList isWorkout={!!workout} exerciseIndex={exerciseIndex} />
-        </Animated.View>
-      )}
+      <View style={{ marginTop: !!workout ? 300 : 0 }}>
+        {!!exercises[exerciseIndex]?.sets && (
+          <Animatable.View style={animatedStyle}>
+            <SetList isWorkout={!!workout} exerciseIndex={exerciseIndex} />
+          </Animatable.View>
+        )}
+      </View>
     </View>
   );
 };
