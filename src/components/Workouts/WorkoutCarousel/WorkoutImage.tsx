f6e4f09d0cedcd0cfead00f46e3dd86e40caa535
Amit Buskila <amit1bus@gmail.com>
2025-03-15 20:26:35 +0200

    feat: add label overlay to workout images in carousel

--- src/components/Workouts/WorkoutCarousel/WorkoutImage.tsx
+++ src/components/Workouts/WorkoutCarousel/WorkoutImage.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from "react";
-import { Image, StyleSheet } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { Pressable } from "react-native-gesture-handler";
 import { screenWidth } from "../../../../constants";
 import { FormExercise } from "../../../contexts/WorkoutForm.context";
@@ -23,6 +23,11 @@ export const WorkoutImage: FC<{ item: FormExercise; index: number }> = ({
       }}
       onPressOut={() => setIsPressing(false)}
     >
+      <View className="absolute top-0.5 left-1/2 -translate-x-1/2 px-2 py-1 rounded-lg z-10 rounded-lg">
+        <Text className="text-black font-bold text-lg bg-[rgba(245,245,245,0.8)]">
+          {item.label}
+        </Text>
+      </View>
       <Image
         className="h-full w-full rounded-lg"
         source={{
