674413d61dcab9f84a34a0fbad9328bf43ca3897
Amit Buskila <amit1bus@gmail.com>
2025-05-15 19:04:13 +0300

    parse input to remove trailing zeros after decimal

--- src/components/GenericComponents/NumericInput.tsx
+++ src/components/GenericComponents/NumericInput.tsx
@@ -67,13 +67,11 @@ export const NumericInput: FC<{
     <View>
       {exercisesState[exerciseIndex].sets[setIndex] && (
         <TextInput
-          value={(
-            exercisesState[exerciseIndex].sets[setIndex][
-              fieldType
-            ]?.toString() ||
-            "" ||
-            ""
-          ).toString()}
+          value={exercisesState[exerciseIndex].sets[setIndex][fieldType]
+            ?.toString()
+            .replace(/(\.\d*?[1-9])0+$/, "$1")
+            .replace(/\.0+$/, "")
+            .replace(/\.$/, "")}
           onChangeText={(value) => {
             setValue(
               `exercises.${exerciseIndex}.sets.${setIndex}.${fieldType}`,
